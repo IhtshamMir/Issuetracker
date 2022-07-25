22f1ca9a5c603627b045bd644b95c29641df7d36
IhtshamMir <94828474+IhtshamMir@users.noreply.github.com>
2022-07-25 23:36:56 +0500

    Add files via upload

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,105 +5,206 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-  /*
-  test('Create an issue with every field',function(){
+
+  test('Create an issue with every field', function() {
     chai
       .request(server)
       .post('/api/issues/apitest')
-      .send( {
-     "issue_title": "Fix error in posting data",
-     "issue_text": "When we post data it has an error.",
-    "created_by": "Joe",
-    "status_text": "In QA"
-       })
-       .end(function (err, res) {
-     assert.equal(res.status, 200, 'Response status should be 200');
-    
+      .send({
+        "issue_title": "Fix error in posting data",
+        "issue_text": "When we post data it has an error.",
+        "created_by": "Joe",
+        "assigned_to": "Doe",
+        "status_text": "In QA"
+      })
+      .end(function(err, res) {
+        assert.equal(res.status, 200, 'Response status should be 200');
       });
-    
+
   })
 
-  
-  test('Create an issue with only required fields',function(){
+
+  test('Create an issue with only required fields', function() {
     chai
       .request(server)
       .post('/api/issues/apitest')
-      .send( {
-     "issue_title": "Fix error in posting data",
-     "issue_text": "When we post data it has an error.",
-    "created_by": "Joe",
-       })
-       .end(function (err, res) {
-     assert.equal(res.status, 200, 'Response status should be 200');
-   
+      .send({
+        "issue_title": "Fix error in posting data",
+        "issue_text": "When we post data it has an error.",
+        "created_by": "Joe",
+      })
+      .end(function(err, res) {
+        assert.equal(res.status, 200, 'Response status should be 200');
+
       });
-    
-  }) 
 
-test('Create an issue with missing required fields: ',function(){
+  })
+
+
+  test('Create an issue with missing required fields: ', function() {
     chai
       .request(server)
       .post('/api/issues/apitest')
-      .send( {
-     "issue_title": "Fix error in posting data",
-     "issue_text": "When we post data it has an error."
-       })
-       .end(function (err, res) {
-         let myobj={ error: 'required field(s) missing' };
-         assert.equal(res.body.toString(),myobj.toString());    
-   });
-    
-  })  
+      .send({
+        "issue_title": "Fix error in posting data",
+        "issue_text": "When we post data it has an error."
+      })
+      .end(function(err, res) {
+        //let myobj={ error: 'required field(s) missing' };
+        // assert.equal(res.body.toString(),myobj.toString());    
+        assert.equal(res.status, 200, 'Response status should be 200');
+      });
 
-test('View issues on a project: GET request  ',function(){
+  })
+
+  test('View issues on a project: GET request  ', function() {
     chai
       .request(server)
       .get('/api/issues/apitest')
-       .end(function (err, res) {
-         assert.equal(res.status, 200, 'Response status should be 200');     
-      
-   });
-    
-  })  
-*/
-  test('DELETE an issue ',function(){
+      .end(function(err, res) {
+        assert.equal(res.status, 200, 'Response status should be 200');
+      });
+
+  })
+
+  test('View issues on a project: GET request with one filter ', function() {
+    chai
+      .request(server)
+      .get('/api/issues/apitest?open=true')
+      .end(function(err, res) {
+        assert.equal(res.status, 200, 'Response status should be 200');
+        console.log("GET with one filter", res.body);
+      });
+
+  })
+  test('View issues on a project: GET request with multiple filter ', function() {
+    chai
+      .request(server)
+      .get('/api/issues/apitest?open=true&issue_title=hellp')
+      .end(function(err, res) {
+        assert.equal(res.status, 200, 'Response status should be 200');
+        console.log("GET with multiple filter", res.body);
+      });
+
+  })
+  test('Update one field on an issue: PUT request to /api/issues/{project} ', function() {
+    chai
+      .request(server)
+      .put('/api/issues/apitest')
+      .send({
+        "_id": "62dd5f02d09668593f805762",
+        "status_text": "In QA"
+
+      })
+      .end(function(err, res) {
+        assert.equal(res.status, 200, 'Response status should be 200');
+        console.log("Update one field with PUT request", res.body);
+      });
+
+  })
+  test('Update multiple fields on an issue: PUT request to /api/issues/{project} ', function() {
+    chai
+      .request(server)
+      .put('/api/issues/apitest')
+      .send({
+        "_id": "62dd5f02d09668593f805762",
+        "issue_text": "Error resolved",
+        "open": "false"
+
+      })
+      .end(function(err, res) {
+        assert.equal(res.status, 200, 'Response status should be 200');
+        console.log("Update multiple fields with PUT request", res.body);
+      });
+
+  })
+
+  test('Update an issue with missing _id: PUT request to  /api/issues/{project} ', function() {
+    chai
+      .request(server)
+      .put('/api/issues/apitest')
+      .send({
+
+        "issue_text": "Error resolved",
+        "open": "false"
+
+      })
+      .end(function(err, res) {
+        assert.equal(res.status, 200, 'Response status should be 200');
+        console.log("Update an issue with missing ID", res.body);
+      });
+
+  })
+  test('Update an issue with no fields to update: PUT request to /api/issues/{project} ', function() {
+    chai
+      .request(server)
+      .put('/api/issues/apitest')
+      .send({
+        "_id": "62dd5f02d09668593f805762"
+
+      })
+      .end(function(err, res) {
+        assert.equal(res.status, 200, 'Response status should be 200');
+        console.log("Update an issue with no updated fields", res.body);
+      });
+
+  })
+
+  test('Update an issue with an invalid _id: PUT request to /api/issues/{project} ', function() {
+    chai
+      .request(server)
+      .put('/api/issues/apitest')
+      .send({
+        "_id": "1234"
+
+      })
+      .end(function(err, res) {
+        assert.equal(res.status, 200, 'Response status should be 200');
+        console.log("Update an issue with invalid ID", res.body);
+      });
+
+  })
+
+
+
+  test('DELETE an issue with invalid ID ', function() {
     chai
       .request(server)
       .delete('/api/issues/apitest')
-    .send( {
-   "_id": "abc124"
-       }) 
-       .end(function (err, res) {
-        assert.equal(res.status,200);
+      .send({
+        "_id": "abc124"
+      })
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
         console.log(res.body);
-   });
-    
-  }) 
-   test('DELETE an issue ',function(){
+      });
+
+  })
+  test('DELETE an issue with Empty ID ', function() {
     chai
       .request(server)
       .delete('/api/issues/apitest')
-    .send( {
-    "_id": ""
-       }) 
-       .end(function (err, res) {
-       assert.equal(res.status,200);
+      .send({
+        "_id": ""
+      })
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
         console.log(res.body);
-   });
-    
-  }) 
-  test('DELETE an issue ',function(){
+      });
+
+  })
+  test('DELETE an issue with complete ID ', function() {
     chai
       .request(server)
       .delete('/api/issues/apitest')
-    .send( {
-    "_id": "62c919046847c795734b1ae0"
-       }) 
-       .end(function (err, res) {
-      assert.equal(res.status,200);
+      .send({
+        "_id": "62dd63225f63280781f60670"
+      })
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
         console.log(res.body);
-   });
-    
-  }) 
-  
+      });
+
+  })
+
 });
